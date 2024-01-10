f870aabd0358e458da2bf8e0a5e81284871144f4
sain0136 <55252053+sain0136@users.noreply.github.com>
2024-01-09 21:24:42 -0500

    feat: Add build tasks and implement user input

--- main.cpp
+++ main.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <time.h>
+#include <stdio.h>
+using namespace std;
+
+struct Person
+{
+    string name;
+    int age;
+    float salary;
+};
+
+void userInput(string gender, Person &p);
 int main()
 {
     /* code */
+    Person person;
+    srand(time(NULL));
+    int token = rand() % 2 + 1;
     struct
     {
-        std::string femaleMessage = "Hi, you are female!";
-        std::string maleMessage = "Hi, you are male!";
+        string femaleMessage = "Hi, Lady!";
+        string maleMessage = "Hey Bro!";
+        string gender = "";
     } messageStruct;
-    std::string gender = "Female";
-    std::cout << "Hello, World!" << std::endl;
-    if (gender == "You are a Male")
-    {
-        std::cout << gender << std::endl;
-    }
-    else if (gender == "Female")
-    {
-        std::cout << "You are a female" << std::endl;
-    }
 
+    messageStruct.gender = (token == 1) ? "Female" : "Male";
+    cout << "Hello, World!" << endl;
+    if (messageStruct.gender == "Male")
+    {
+        cout << messageStruct.maleMessage << endl;
+    }
+    else if (messageStruct.gender == "Female")
+    {
+        cout << messageStruct.femaleMessage << endl;
+    }
+    userInput(messageStruct.gender, person);
+    cout << "Bye, " << person.name << endl;
     return 0;
+}
+
+void userInput(string gender, Person &p) // Passing by refrence here cool
+{
+    string name = "";
+    printf("Welcome, your gender was %s \n", gender.c_str()); // makes a c style char pointer since printf is c func
+    cout << "Tell us your fullname" << endl;
+    cin >> name;
+    cout << "Cool nice to meet you:" << name << endl;
+    p.name = name;
 }
